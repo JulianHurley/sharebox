160b263eb0ed2b3ae9bb71a59ca7d628581ce74a
Julian Hurley <julian@designimperial.com>
2014-06-22 23:01:15 +0100

    implement sign out

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -2,18 +2,27 @@ require 'rails_helper'
 
 feature 'User signup', js: true, type: :feature do
 
-
-	scenario 'user signs up' do
+	def sign_in
 		visit '/users/sign_up'
 		fill_in 'user_name', with: 'name'
 		fill_in 'user_email', with: 'myemail@email.com'
 		fill_in 'user_password', with: 'passpass'
 		fill_in 'user_password_confirmation', with: 'passpass'
-
 		click_button 'Sign up'
-		
+	end
+
+	it 'user signs up' do
+		sign_in
+
 		expect(page).to have_text('Home')
 		expect(page).to have_text('Welcome! You have signed up successfully.')
 		expect(page).to have_text('name')
+	end
+
+	it 'user can sign out' do
+		sign_in
+		click_link 'sign out'
+
+		expect(page).to have_text('Signed out successfully.')
 	end
 end
